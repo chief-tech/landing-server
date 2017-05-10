d37315fd708e920b105790bef301a4e6ee3c83d7
Isaac Zinda <isaaczinda@WL-223-165.WPA.Claremont.Edu>
2017-05-10 12:07:58 -0700

    move to event-driven errors

--- sql-test.js
+++ sql-test.js
@@ -1,10 +1,39 @@
 var mysql = require('mysql');
 var fs = require('fs');
 
+var events = require('events');
+
+var errorEvents = new events.EventEmitter();
+var successEvents = new events.EventEmitter();
+
 // Require `PhoneNumberFormat`.
 var PNF = require('google-libphonenumber').PhoneNumberFormat;
 var phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
 
+// setup all of the fun listners
+errorEvents.addListener('invalid-number', function() {
+  console.log("invalid phone number");
+});
+
+errorEvents.addListener('invalid-data', function() {
+  console.log("invalid data");
+});
+
+errorEvents.addListener('number-unavailable', function() {
+  console.log("the number you tried to register for has already been registered.");
+});
+
+errorEvents.addListener('general-error', function(description) {
+  console.log("error: " + description);
+});
+
+successEvents.addListener('user-added', function() {
+  console.log("user was added successfully");
+});
+
+successEvents.addListener('photo-changed', function() {
+  console.log("photo was changed successfully");
+});
 
 function sqlConnect(callback)
 {
@@ -37,7 +66,7 @@ function sqlConnect(callback)
 }
 
 // returns null if the phone number is invalid
-function condensePhoneNumber(phoneNumber)
+function formatPhoneNumber(phoneNumber)
 {
   // parse the phone number as a US number
   var parsedPhoneNumber = phoneUtil.parse(phoneNumber, 'US')
@@ -54,18 +83,53 @@ function condensePhoneNumber(phoneNumber)
 }
 
 // checks is a phone number is valid
-function checkPhoneNumber(phoneNumber)
+function validPhoneNumber(phoneNumber)
 {
   // if the phone number is null
-  if (!condensePhoneNumber(phoneNumber))
+  if (!formatPhoneNumber(phoneNumber))
   {
     return false;
   }
   return true;
 }
 
+// checks an object to make sure that it is valid
+function validateData(data)
+{
+  // make sure that the data contains all the right keys
+  ['PhoneNumber', 'LastName', 'FirstName', 'Birthday'].forEach(function(element){
+    if (!(element in data))
+    {
+      return false;
+    }
+  });
+
+  // make sure that all values are in the correct format
+
+  return true;
+}
+
 // calls back true if data was added, false if phone # was already present
-function addData(data, connection, callback) {
+function addUser(connection, data) {
+  // make sure that the data is valid
+  if(!validateData(data)) // if the data is invalid
+  {
+    errorEvents.emit('invalid-data');
+    return;
+  }
+
+  data["ProfilePicturePath"] = "default.png"; // set the default user photo
+
+  // if the phone number passed is not valid
+  if (!validPhoneNumber(data.PhoneNumber))
+  {
+    errorEvents.emit('invalid-number');
+    return;
+  }
+
+  // format the phone number to the international standard
+  data.PhoneNumber = formatPhoneNumber(data.PhoneNumber);
+
   connection.query('INSERT INTO Drivers SET ?', data, function(error, results, fields) {
     // if there was an error inserting, check to see if the phone number already exists
     if (error)
@@ -80,51 +144,64 @@ function addData(data, connection, callback) {
 
         if (results.length == 1) // if there was already an entry
         {
-          callback(null, false); //notify the user that there was already an entry
+          errorEvents.emit('number-unavailable'); //notify the user that there was already an entry
           return;
         }
         else { // there was some really serious problem
-          throw new Error("there was a really serious problem");
+          errorEvents.emit('general-error', 'server-side problem with add user query');
           return;
         }
       });
 
     // if there was no error inserting
     } else {
-      callback(null, true); //it all worked
+      successEvents.emit('user-added'); //it all worked
       return;
     }
   });
 }
 
+// returns true if photo number exists
+function changePhoto(connection, phoneNumber, filepath)
+{
+  // if the phone number passed is not valid
+  if (!validPhoneNumber(phoneNumber))
+  {
+    errorEvents.emit('invalid-number');
+    return;
+  }
 
-//etSupportedRegions();
-console.log(checkPhoneNumber('+49 206 915 2306'));
-console.log(checkPhoneNumber('+4 206 915 2306'));
-console.log(checkPhoneNumber('206 915 2306'));
-console.log(checkPhoneNumber('206-915-2306'));
-console.log(checkPhoneNumber('12069152306'));
+  // format the phone number to the international standard
+  phoneNumber = formatPhoneNumber(phoneNumber);
 
+  connection.query('UPDATE Drivers SET ? WHERE PhoneNumber = ?', [{ProfilePicturePath: filepath}, phoneNumber], function(error, results, fields) {
+    // if there was an error inserting, check to see if the phone number already exists
+    if (error)
+    {
+      errorEvents.emit('general-error', 'query to the database failed');
+      return;
+    }
 
-// sqlConnect(function(connection, err) {
-//   if (err) {
-//     return;
-//   }
-//
-//   data = { PhoneNumber: "206 915 2436", FirstName: 'Isaac', LastName: 'Zinda', Birthday: '1998-05-02' };
-//
-//   addData(data, connection, function(err, added) {
-//     console.log(err);
-//     console.log(added);
-//
-//     // if (err)
-//     // {
-//     //   console.error(err);
-//     //   return;
-//     // }
-//     // else
-//     // {
-//     //   console.log(added);
-//     // }
-//   });
-// });
+    if (results.affectedRows == 0) // if no rows were effected, the phone number does not exist
+    {
+      errorEvents.emit('general-error', 'the specified phone number has not been registered.');
+      return;
+    }
+    else // if some rows were effected, the phone number does exist
+    {
+      successEvents.emit('photo-changed');
+      return;
+    }
+  });
+}
+
+sqlConnect(function(connection, err) {
+  if (err) {
+    return;
+  }
+
+  changePhoto(connection, "206 917 2306", "helloworld.txt");
+
+  // data = { PhoneNumber: "206 915 2366", FirstName: 'Isaac', LastName: 'Zinda', Birthday: '1998-05-02' };
+  // addUser(connection, data);
+});
