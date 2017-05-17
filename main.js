2c2fa8e351de9a6b0d53dd9f37558e41ec9214ea
Isaac Zinda <isaaczinda@isaac-macbookpro-2.local>
2017-05-16 19:37:25 -0700

    implement socket.io interface

--- main.js
+++ main.js
@@ -1,14 +1,69 @@
+// for HTTP
 var io = require('socket.io').listen(8080);
+
+// for SSL
+// var fs = require('fs');
+// var https = require('https');
+// var express = require('express');
+// var app = express();
+// var options = {
+//   key: fs.readFileSync('./passwords/ssl/domain.pem'),
+//   cert: fs.readFileSync('./passwords/ssl/domain.crt')
+// };
+// // var serverPort = 443;
+// var server = https.createServer(options, app);
+// var io = require('socket.io')(server);
+//
+// server.listen(443);
+//
+// app.get('/', function (req, res) {
+//   res.send('hello world');
+// });
+// end
+
 var fs = require('fs');
+var sql = require('./sql.js');
 
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
+// processes error messages
+sql.events.addListener('warning', function(data) {
+  var socket = data.socket;
+  delete data.socket; //make sure socket it not sent
+  console.error(data);
+
+  if (data.data) {
+    socket.emit('warning', data);
+  } else {
+    socket.emit('warning', data.name);
+  }
+});
+
+// processes info messages
+sql.events.addListener('info', function(data) {
+  var socket = data.socket;
+  delete data.socket; //make sure that socket is not sent
+  console.log(data);
+
+  if (data.data) {
+    socket.emit('info', data);
+  } else {
+    socket.emit('info', data.name);
+  }
+});
 
 
+// when we establish a new connection
+io.sockets.on('connection', function (socket)
+{
+  socket.emit('request-add-user'); // tests the add user functionality
 
   socket.on('add-user', function (data)
   {
-    console.log(data);
+    sql.addUser(socket, data);
+  });
+
+  socket.on('change-photo', function(data)
+  {
+    sql.changePhoto(socket, data);
   });
 });
--- sql.js
+++ sql.js
@@ -1,86 +1,56 @@
 var mysql = require('mysql');
 var fs = require('fs');
 var sqlCredentials = require('./passwords/sql.json');
-var events = require('events');
+var eventLibrary = require('events');
 var phone = require('./phone.js');
 
-var errorEvents = new events.EventEmitter();
-var successEvents = new events.EventEmitter();
-
-// setup all of the fun listners
-errorEvents.addListener('invalid-number', function() {
-  console.log("invalid phone number");
-});
-
-errorEvents.addListener('invalid-data', function() {
-  console.log("invalid data");
-});
-
-errorEvents.addListener('number-unavailable', function() {
-  console.log("the number you tried to register for has already been registered.");
-});
-
-errorEvents.addListener('general-error', function(description) {
-  console.log("error: " + description);
-});
-
-successEvents.addListener('user-added', function() {
-  console.log("user was added successfully");
-});
-
-successEvents.addListener('photo-changed', function() {
-  console.log("photo was changed successfully");
-});
+// setup event factory
+var events = new eventLibrary.EventEmitter();
 
 // connect to the mysql server
-sqlConnect();
+var connection = mysql.createConnection({
+  host     : sqlCredentials['host'],
+  user     : sqlCredentials['user'],
+  password : sqlCredentials['password'],
+  database : 'landing'
+});
 
-// connect to the sql table
-function sqlConnect()
-{
-    // setup connection specifics
-    var connection = mysql.createConnection({
-      host     : sqlCredentials['host'],
-      user     : sqlCredentials['user'],
-      password : sqlCredentials['password'],
-      database : 'landing'
-    });
+// connect to the server
+connection.connect();
 
-    // connect to the server
-    connection.connect();
+
+// creates an object from some data and a socket
+function toEventObject(socket, name, data) {
+  return {"socket": socket, "name": name, "data": data};
 }
 
-// checks an object to make sure that it is valid
-function validateData(data)
-{
-  // make sure that the data contains all the right keys
-  ['PhoneNumber', 'LastName', 'FirstName', 'Birthday'].forEach(function(element){
-    if (!(element in data))
-    {
-      return false;
-    }
-  });
-
-  // make sure that all values are in the correct format
-
-  return true;
+function toEventObject(socket, name) {
+  return {"socket": socket, "name": name, "data": null};
 }
 
 // calls back true if data was added, false if phone # was already present
-function addUser(connection, data) {
-  // make sure that the data is valid
-  if(!validateData(data)) // if the data is invalid
-  {
-    errorEvents.emit('invalid-data');
-    return;
+var addUser = function(socket, data) {
+  // make sure that the data contains the right keys
+  ['PhoneNumber', 'LastName', 'FirstName', 'Birthday'].forEach(function(element){
+    if (!(element in data))
+    {
+      events.emit('warning', toEventObject(socket, 'invalid-data'));
+      return;
+    }
+  });
+
+  // make sure that birthday is 8 numbers long
+  if (!data.PhoneNumber.match('^[0-9]{8}$')) {
+    events.emit('warning', toEventObject(socket, 'invalid-data', 'birthday was incorrectly formatted'));
   }
+
 
   data["ProfilePicturePath"] = "default.png"; // set the default user photo
 
   // if the phone number passed is not valid
   if (!phone.validate(data.PhoneNumber))
   {
-    errorEvents.emit('invalid-number');
+    events.emit('warning', toEventObject(socket, 'invalid-number'));
     return;
   }
 
@@ -95,36 +65,42 @@ function addUser(connection, data) {
       connection.query('SELECT PhoneNumber FROM Drivers WHERE PhoneNumber = ?', [data.PhoneNumber], function(error, results, fields) {
         if (error)
         {
-          callback(error);
+          events.emit('warning', toEventObject(socket, 'general-error', error.message));
           return;
         }
 
         if (results.length == 1) // if there was already an entry
         {
-          errorEvents.emit('number-unavailable'); //notify the user that there was already an entry
+          events.emit('warning', toEventObject(socket, 'number-unavailable')); //notify the user that there was already an entry
           return;
         }
         else { // there was some really serious problem
-          errorEvents.emit('general-error', 'server-side problem with add user query');
+          events.emit('warning', toEventObject(socket, 'general-error', 'server-side problem with add user query'));
           return;
         }
       });
 
     // if there was no error inserting
     } else {
-      successEvents.emit('user-added'); //it all worked
+      events.emit('info', toEventObject(socket, 'user-added')); //it all worked
       return;
     }
   });
 }
 
 // returns true if photo number exists
-function changePhoto(connection, phoneNumber, filepath)
+var changePhoto = function(socket, data)
 {
+  // make sure the correct values are in the data
+  if (!("ProfilePicturePath" in data && "PhoneNumber" in data))
+  {
+    events.emit('warning', toEventObject(socket, 'invalid-data'));
+  }
+
   // if the phone number passed is not valid
   if (!phone.validate(phoneNumber))
   {
-    errorEvents.emit('invalid-number');
+    events.emit('warning', toEventObject(socket, 'invalid-number'));
     return;
   }
 
@@ -135,31 +111,23 @@ function changePhoto(connection, phoneNumber, filepath)
     // if there was an error inserting, check to see if the phone number already exists
     if (error)
     {
-      errorEvents.emit('general-error', 'query to the database failed');
+      events.emit('warning', toEventObject(socket, 'general-error', 'query to the database failed'));
       return;
     }
 
     if (results.affectedRows == 0) // if no rows were effected, the phone number does not exist
     {
-      errorEvents.emit('general-error', 'the specified phone number has not been registered.');
+      events.emit('warning', toEventObject(socket, 'general-error', 'the specified phone number has not been registered.'));
       return;
     }
     else // if some rows were effected, the phone number does exist
     {
-      successEvents.emit('photo-changed');
+      events.emit('info', 'photo-changed', toEventObject(socket));
       return;
     }
   });
 }
 
-
-// sqlConnect(function(connection, err) {
-//   if (err) {
-//     return;
-//   }
-//
-//   changePhoto(connection, "206 917 2306", "helloworld.txt");
-//
-//   // data = { PhoneNumber: "206 915 2366", FirstName: 'Isaac', LastName: 'Zinda', Birthday: '1998-05-02' };
-//   // addUser(connection, data);
-// });
+module.exports.changePhoto = changePhoto;
+module.exports.addUser = addUser;
+module.exports.events = events;
